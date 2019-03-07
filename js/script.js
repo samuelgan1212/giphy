d080ebc5612436949ee9d987e7a5964a12c55da8
Huizhuo Gan <jokerandjoe@gmail.com>
2019-03-07 01:55:47 +0000

    add some javascript

--- js/script.js
+++ js/script.js
@@ -4,8 +4,17 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
+    $('.gallery').empty();
+    var searchTerm = $('#search-term').val();
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+    url:url,
+        method: "GET",
+        success: function(response) {
+            $(".row gallery justify-content-center").append("<a href='"+response["data"]["images"]["fixed_width"]["url"]+"'>/<img src="+response["data"]["images"]["fixed_width"]["url"]+);
+            }
+    
+    });
   
 });
 
