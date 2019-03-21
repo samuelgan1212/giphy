59f8035d374a931289d142630bc00f23ccc8d023
Huizhuo Gan <jokerandjoe@gmail.com>
2019-03-21 00:31:58 +0000

    add some java

--- js/script.js
+++ js/script.js
@@ -19,4 +19,45 @@ $("#search-button").click(function(){
     });
   
 });
+$("#Search-buttonallimage").click(function(){
+    $('.gallery').empty();
+    var searchTerm = $('#search-term').val();
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+    url:url,
+        method: "GET",
+        success: function(response) {
+            var array=response["data"];
+            var newHTML=[];
+            for (var i = 0; i < array.length; i++) {
+    newHTML.push("<a href =" +response.data[ Math.floor(Math.random() * response.data.length)].images.fixed_width.url+"><img src='" + response.data[ Math.floor(Math.random() * response.data.length)].images.fixed_width.url + "'></a>");
+}
+            $(".gallery").html(
+                newHTML.join("")
+                );
+            }
+    
+    });
+  
+});
+
+
+
+  $('#emailLink').on('click', function (event) {
+    var searchTerm = $('#search-term').val();
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+    url:url,
+        method: "GET",
+        success: function(response) {
+     event.preventDefault();
+    alert("Warning!!! Switch TO Gmail!!!!");
+    var email = 'TARGET@theearth.com';
+    var subject = 'GIF THAT YOU WILL LIKE!!!!';
+    var emailBody =  response.data[Math.floor(Math.random() * response.data.length)].images.fixed_width.url;
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+        }
+    
+    }); 
+  });
 
